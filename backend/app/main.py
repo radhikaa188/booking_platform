b76cb0f112ad365edd01cc1c3ee25c9067acadc6
Radhika Agarwal <radhikaag18@gmail.com>
2026-07-26 23:36:47 +0530

    allow vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(title="Event Booking Platform")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000","https://booking-platform-five-plum.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
