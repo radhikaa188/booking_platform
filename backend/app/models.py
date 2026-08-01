7745cbef4e8c8440a4946398408cebd8f0b7a12e
Radhika Agarwal <radhikaag18@gmail.com>
2026-08-02 01:17:07 +0530

    enum values mismatched

--- backend/app/models.py
+++ backend/app/models.py
@@ -14,7 +14,7 @@ class Venue(Base):
 class ScreenType(str, enum.Enum):
     standard = "standard"
     imax = "imax"
-    fourdx = "4dx"
+    4dx = "4dx"
 class Screen(Base):
     __tablename__= "screens"
 
